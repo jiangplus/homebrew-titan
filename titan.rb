97f6db118e005665b2d8b399b9628f062dcb5249
jiangplus <jiang.plus.times@gmail.com>
2020-09-18 17:03:15 +0800

    add v0.5.4

---
+++ titan.rb
@@ -0,0 +1,18 @@
+class Titan < Formula
+  desc ""
+  homepage ""
+  url "http://titan.s3.cn-northwest-1.amazonaws.com.cn/brew/titan-v0.5.4"
+  version "0.5.4"
+  sha256 "6640d9bbff146b9a4180658be9db5b45dbf8cc597b880e0180fda971b116cf4f"
+
+  depends_on "erlang"
+
+  def install
+    bin.install "titan"
+  end
+
+  test do
+    system "false"
+  end
+end
+
